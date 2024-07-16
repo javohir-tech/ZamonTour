a174e929dfebb131d3e988fafa4a4f8e433fc3ef
Suvonov Javohir <suvonovjavohir625@gmail.com>
2024-07-16 16:27:31 +0500

    minor bugs fixed

--- src/Sections/Suggestions/Suggestions.jsx
+++ src/Sections/Suggestions/Suggestions.jsx
@@ -29,12 +29,12 @@ import { useTranslation } from 'react-i18next';
 
 
 export default function Suggestions() {
-  const {t} = useTranslation()
+  const { t } = useTranslation()
   return (
     <div className="container py-5" id='tours'>
       <div className='suggestions-section py-5'>
         {/* section header */}
-        <SectionHeader infoTitle={t('suggestions-section__title')} infoSubtile={t('suggestions-section__subtitle')} textPosition={'text-center mb-5'}/>
+        <SectionHeader infoTitle={t('suggestions-section__title')} infoSubtile={t('suggestions-section__subtitle')} textPosition={'text-center mb-5'} />
         {/* swiper card */}
         <div className='suggestions-section__cards'>
           <Swiper
@@ -45,26 +45,30 @@ export default function Suggestions() {
               disableOnInteraction: false,
             }}
             breakpoints={{
-              768: {
+              996: {
                 slidesPerView: 3,
+              },
+              768: {
+                slidesPerView: 2,
               },
               366: {
                 slidesPerView: 1,
               },
             }}
+            loop={true} 
             navigation={true}
             modules={[Autoplay, Navigation]}
             className="mySwiper"
           >
-            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2'/>} cardIcon_2={<AirplaneFill className='me-2'/>} cardIcon_1={<CarFrontFill className='me-2'/>} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={antalya} city={t('antalya')} summa={'$999'} clients={"500"}/></SwiperSlide>
-            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2'/>} cardIcon_2={<AirplaneFill className='me-2'/>} cardIcon_1={<CarFrontFill className='me-2'/>} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={first} city={t('sharm')} summa={'$800'} clients={'200'}/></SwiperSlide>
-            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2'/>} cardIcon_2={<AirplaneFill className='me-2'/>} cardIcon_1={<CarFrontFill className='me-2'/>} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={usa} city={t('usa')} clients={"434"}/></SwiperSlide>
-            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2'/>} cardIcon_2={<AirplaneFill className='me-2'/>} cardIcon_1={<CarFrontFill className='me-2'/>} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={angliya} city={t('england')} clients={'300'}/></SwiperSlide>
-            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2'/>} cardIcon_2={<AirplaneFill className='me-2'/>} cardIcon_1={<CarFrontFill className='me-2'/>} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={kanada} city={t('kanada')} clients={'300'}/></SwiperSlide>
-            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2'/>} cardIcon_2={<AirplaneFill className='me-2'/>} cardIcon_1={<CarFrontFill className='me-2'/>} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={yevropa} city={t('yevropa')} clients={'360'}/></SwiperSlide>
-            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2'/>} cardIcon_2={<AirplaneFill className='me-2'/>} cardIcon_1={<CarFrontFill className='me-2'/>} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={istanbul} city={t('istanbul')} clients={'234'} summa={'$420'}/></SwiperSlide>
-            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2'/>} cardIcon_2={<AirplaneFill className='me-2'/>} cardIcon_1={<CarFrontFill className='me-2'/>} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={dubai} city={t('dubai')} clients={'300'} summa={'$700'}/></SwiperSlide>
-          </Swiper> 
+            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2' />} cardIcon_2={<AirplaneFill className='me-2' />} cardIcon_1={<CarFrontFill className='me-2' />} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={antalya} city={t('antalya')} summa={'$999'} clients={"500"} /></SwiperSlide>
+            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2' />} cardIcon_2={<AirplaneFill className='me-2' />} cardIcon_1={<CarFrontFill className='me-2' />} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={first} city={t('sharm')} summa={'$800'} clients={'200'} /></SwiperSlide>
+            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2' />} cardIcon_2={<AirplaneFill className='me-2' />} cardIcon_1={<CarFrontFill className='me-2' />} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={usa} city={t('usa')} clients={"434"} /></SwiperSlide>
+            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2' />} cardIcon_2={<AirplaneFill className='me-2' />} cardIcon_1={<CarFrontFill className='me-2' />} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={angliya} city={t('england')} clients={'300'} /></SwiperSlide>
+            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2' />} cardIcon_2={<AirplaneFill className='me-2' />} cardIcon_1={<CarFrontFill className='me-2' />} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={kanada} city={t('kanada')} clients={'300'} /></SwiperSlide>
+            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2' />} cardIcon_2={<AirplaneFill className='me-2' />} cardIcon_1={<CarFrontFill className='me-2' />} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={yevropa} city={t('yevropa')} clients={'360'} /></SwiperSlide>
+            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2' />} cardIcon_2={<AirplaneFill className='me-2' />} cardIcon_1={<CarFrontFill className='me-2' />} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={istanbul} city={t('istanbul')} clients={'234'} summa={'$420'} /></SwiperSlide>
+            <SwiperSlide className='suggestion-card__swiper'><SuggestionsCard cardIcon_3={<BuildingFill className='me-2' />} cardIcon_2={<AirplaneFill className='me-2' />} cardIcon_1={<CarFrontFill className='me-2' />} clientsTitle={t('clienst')} cardTitle={t('card-title')} cardItem_1={t('hotel-info')} cardItem_2={t('aviabiletlar')} cardItem_3={t('location')} images={dubai} city={t('dubai')} clients={'300'} summa={'$700'} /></SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </div>
